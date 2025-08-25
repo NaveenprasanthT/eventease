2c92b1eeea9cc7ce9f49a41568e20926033aafb2
NaveenprasanthT <naveen.t@in.aictec.com>
2025-08-25 19:14:50 +0530

    final fix

--- prisma/migrations/20250823153234_add_role_to_user/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "public"."Role" AS ENUM ('ADMIN', 'STAFF', 'EVENT_OWNER');
-
--- AlterTable
-ALTER TABLE "public"."user" ADD COLUMN     "role" "public"."Role" DEFAULT 'STAFF';
--- prisma/migrations/20250823173532_add_attendee_fields/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."event" ADD COLUMN     "attendeeCount" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "maxAttendeeCount" INTEGER;
