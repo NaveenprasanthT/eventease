1196bc1d43ed4c76eee54315ac949423c4db1372
NaveenprasanthT <naveen.t@in.aictec.com>
2025-08-25 19:23:07 +0530

    final fix 2

--- app/api/public/route.ts
+++ app/api/public/route.ts
@@ -3,11 +3,13 @@ import { prisma } from "@/lib/prisma";
 
 // List all events
 export async function GET(req: NextRequest) {
+  console.log(req);
   let filter = {
     include: { owner: true, rsvps: true },
   };
 
   const events = await prisma.event.findMany(filter);
+  console.log(events, 'events-----------------');
 
   // add attendeeCount to each event
   const eventsWithCount = events.map((event: any) => ({
